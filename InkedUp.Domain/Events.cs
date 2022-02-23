d35f64874195f95c668e0a22c703057abe3aed23
Phil <philliptjohnson@hotmail.com>
2022-02-23 08:51:20 +0000

    Added Events
    Added Events that map exactly to the domain model

---
+++ InkedUp.Domain/Events.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace InkedUp.Domain
+{
+    public static class Events
+    {
+        public class PenCreated
+        {
+            public Guid Id { get; set; }
+            public Guid OwnerId { get; set; }
+        }
+
+        public class BottleOfInkCreated
+        {
+            public Guid Id { get; set; }
+            public Guid OwnerId { get; set; }
+        }
+
+        public class PenManufacturerUpdated
+        {
+            public Guid Id { get; set; }
+            public string Manufacturer { get; set; }
+        }
+
+        public class PenModelUpdated
+        {
+            public Guid Id { get; set; }
+            public string Model { get; set; }
+        }
+
+        public class BottleOfInkManufacturerUpdated
+        {
+            public Guid Id { get; set; }
+            public string Manufacturer { get; set; }
+        }
+
+        public class BottleOfInkModelUpdated
+        {
+            public Guid Id { get; set; }
+            public string Model { get; set; }
+        }
+
+        public class PenInkedUp
+        {
+            public Guid Id { get; set; }
+            public string InkName { get; set; }
+        }
+        
+        public class PenFlushed
+        {
+            public Guid Id { get; set; }
+        }
+        
+        public class InkDeleted
+        {
+            public Guid Id { get; set; }
+        }
+        
+        public class PenDeleted
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
